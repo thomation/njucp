379849cbb7d1ae18fced1599980689974db7da36
Thomation <thomation@126.com>
2024-03-17 12:10:17 +0800

    Add a simple loop of lexer

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,30 @@
-import java.io.*; 
+import java.util.List;
+
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.Token;
 public class Main
 {    
+    @SuppressWarnings("deprecation")
     public static void main(String[] args){
-        String fileName = args[0]; 
-        try {
-            BufferedReader in = new BufferedReader(new FileReader(fileName));
-            String str;
-            while ((str = in.readLine()) != null) {
-                System.out.println(str);
-            }
-        } catch (IOException e) {
+         if (args.length < 1) {
+            System.err.println("input path is required");
         }
+        String source = args[0];
+        try{
+            CharStream input = CharStreams.fromFileName(source);
+            SysYLexerLexer sysYLexer = new SysYLexerLexer(input);
+            // sysYLexer.removeErrorListeners();
+            // sysYLexer.addErrorListener(myErrorListener);
+            List<? extends Token> myTokens = sysYLexer.getAllTokens();
+            for (Token t : myTokens) {
+                printSysYTokenInformation(t, sysYLexer.getTokenNames());
+            }
+        } catch(Exception e) {
+            System.err.println(e.getMessage());
+        }
+    }
+    static void printSysYTokenInformation(Token t, String[] names) {
+        System.out.println(String.format("%s %s at Line %d",names[t.getType()], t.getText(), t.getLine()));
     }
 }
