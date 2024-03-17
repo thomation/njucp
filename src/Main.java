9cf421cfb5ea4bf73137f1397398cc35ef377888
Thomation <thomation@126.com>
2024-03-17 18:47:22 +0800

    Convert hex to oct

--- src/Main.java
+++ src/Main.java
@@ -32,10 +32,19 @@ public class Main {
 
     static void printSysYTokenInformation(Token t, Vocabulary v) {
         String symbolName = v.getSymbolicName(t.getType());
-        // if(symbolName == "INTEGER_CONST") {
-        //     System.out.println(String.format("%s %d at Line %d.", symbolName, Integer.parseInt(t.getText()), t.getLine()));
-        // } else {
-            System.out.println(String.format("%s %s at Line %d.", symbolName, t.getText(), t.getLine()));
-        // }
+        String text = t.getText();
+        if (symbolName == "INTEGER_CONST") {
+            int intValue = 0;
+            if (text.length() > 1 && text.charAt(0) == '0' && (text.charAt(1) == 'x' || text.charAt(1) == 'X')) {
+                intValue = Integer.parseInt(text.substring(2), 16);
+            } else if (text.length() > 1 && text.charAt(0) == '0') {
+                intValue = Integer.parseInt(text.substring(1), 8);
+            } else {
+                intValue = Integer.parseInt(text);
+            }
+            System.out.println(String.format("%s %d at Line %d.", symbolName, intValue, t.getLine()));
+        } else {
+            System.out.println(String.format("%s %s at Line %d.", symbolName, text, t.getLine()));
+        }
     }
 }
